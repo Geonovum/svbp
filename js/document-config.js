70a65a8a558df2677b837100a2ae864f972269dc
Monique van Scherpenzeel <m.vanscherpenzeel@geonovum.nl>
2023-11-29 14:06:31 +0100

    aanpassing specType

--- js/document-config.js
+++ js/document-config.js
@@ -4,7 +4,7 @@ var documentConfig =
   shortName: "svbp",
   pubDomain: "ro",
   specStatus: "def",
-  specType: "no",
+  specType: "st",
   license: "cc-by-nd",
   latestVersion: [
     "https://docs.geostandaarden.nl/ro/svbp/"
